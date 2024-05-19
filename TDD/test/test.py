a0f6ef2075f5714f29dd04abf320732f2c29374f
RamtinMoslemi <ramtin4moslemi@gmail.com>
2024-05-19 16:21:57 +0330

    Add tests

--- TDD/test/test.py
+++ TDD/test/test.py
@@ -1,11 +1,12 @@
 import unittest
-from classes.Library import Library
-from classes.Book import Book
-from classes.Student import Student
+from library import Library
+from book import Book
+from student import Student
 
 
 class SearchTestCase(unittest.TestCase):
     def test_book_search(self):
+        # Arrange
         library = Library()
 
         book1 = Book(1, 'George Orwell', '1984')
@@ -14,40 +15,46 @@ class SearchTestCase(unittest.TestCase):
         book4 = Book(4, 'Sun Szu', 'The Art of War')
         book5 = Book(5, 'Sir Arthur Conan Doyle', 'The Sign of Four')
 
+        # Act
         library.add_book(book1)
         library.add_book(book2)
         library.add_book(book3)
         library.add_book(book4)
 
-        self.assertFalse(book5 in library.books, 'unregistered book')
         author_search = library.search_books(['George Orwell'], 'author')
-        self.assertListEqual(author_search, [book1, book2], 'search by author')
-        author_search = library.search_books(['Sir Arthur Conan Doyle'], 'author')
-        self.assertListEqual(author_search, [book3], 'search by author (unregistered)')
         id_search = library.search_books([1, 2, 3], 'id')
-        self.assertListEqual(id_search, [book1, book2, book3], 'search by id')
         title_search = library.search_books(['The Great Gatsby', 'Catch-22'], 'title')
-        self.assertListEqual(title_search, list(), 'search by name')
+
+        # Assert
+        self.assertFalse(book5 in library.books, 'unregistered book')
+        self.assertListEqual(author_search, [book1, book2], 'search by author')
+        self.assertListEqual(id_search, [book1, book2, book3], 'search by id')
+        self.assertListEqual(title_search, list(), 'search by title')
 
     def test_student_search(self):
+        # Arrange
         library = Library()
 
         student1 = Student(1, 'Alice')
         student2 = Student(2, 'Bob')
         student3 = Student(3, "John")
 
+        # Act
         library.add_student(student1)
         library.add_student(student2)
         library.add_student(student3)
 
         id_search = library.search_students([1, 2], 'id')
-        self.assertListEqual(id_search, [student1, student2], 'search by id')
         name_search = library.search_students(['Alice', 'Bob', 'David'], 'name')
+
+        # Assert
+        self.assertListEqual(id_search, [student1, student2], 'search by id')
         self.assertListEqual(name_search, [student1, student2], 'search by name')
 
 
 class LendingTestCase(unittest.TestCase):
     def test_lending(self):
+        # Arrange
         library = Library()
 
         book1 = Book(1, 'George Orwell', '1984')
@@ -61,6 +68,7 @@ class LendingTestCase(unittest.TestCase):
         student3 = Student(3, "John")
         student4 = Student(4, "David")
 
+        # Act
         library.add_book(book1)
         library.add_book(book2)
         library.add_book(book3)
@@ -71,6 +79,7 @@ class LendingTestCase(unittest.TestCase):
         library.add_student(student2)
         library.add_student(student3)
 
+        # Assert
         self.assertTrue(library.lend_book(book1, student1), 'lending')
         self.assertTrue(library.return_book(book1, student1), 'returning')
         self.assertTrue(library.lend_book(book1, student3), 're-lending')
