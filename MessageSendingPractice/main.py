13f00e4d603bce57238c2071899226eb683bb820
RamtinMoslemi <ramtin4moslemi@gmail.com>
2024-05-21 00:28:27 +0330

    Implement main

--- MessageSendingPractice/main.py
+++ MessageSendingPractice/main.py
@@ -1,3 +1,21 @@
+from services import EmailMessageService, SmsMessageService
+from message import Message
+
 if __name__ == '__main__':
-    print('Hello and Welcome to SE Lab Messenger.')
-    
+    print('Hello and Welcome to SE Lab Messenger.\n')
+
+    while True:
+        command = input('In order to send SMS message enter 1\n'
+                        'In order to sent Email message enter 2\n'
+                        'In order to exit, enter 0\n')
+
+        if command == '0':
+            break
+        elif command == '1' or command == '2':
+            port_type = 'phone number' if command == '1' else 'email'
+            message_service = SmsMessageService() if command == '1' else EmailMessageService()
+            source = input(f'Enter source {port_type}: ')
+            target = input(f'Enter target {port_type}: ')
+            content = input('Write your message:\n')
+            message = Message(source=source, target=target, content=content)
+            message_service.send_message(message)
--- MessageSendingPractice/services.py
+++ MessageSendingPractice/services.py
@@ -1,30 +1,42 @@
+import re
+
+
 class MessageService:
     def send_message(self, message):
         raise NotImplemented
 
-    def validate(self, access_point):
+    def validate(self, port):
         raise NotImplemented
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}()'
 
 
 class SmsMessageService(MessageService):
+    phone_regex = r'[0-9]+'
+
     def send_message(self, message):
         if self.validate(message.source) and self.validate(message.target):
             print('Sending an SMS from ' + message.source + ' to ' +
                   message.target + ' with content ' + message.content)
+        else:
+            print('Invalid phone number!')
 
-    def validate(self, access_point):
-        if len(access_point) != 11:
+    def validate(self, port):
+        if len(port) != 11:
             return False
-        return True
+        return re.fullmatch(self.phone_regex, port)
 
 
 class EmailMessageService(MessageService):
+    email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+
     def send_message(self, message):
         if self.validate(message.source) and self.validate(message.target):
             print('Sending an Email from ' + message.source + ' to ' +
                   message.target + ' with content ' + message.content)
+        else:
+            print('Invalid email!')
 
-    def validate(self, access_point):
-        if len(access_point) != 11:
-            return False
-        return True
+    def validate(self, port):
+        return re.fullmatch(self.email_regex, port)
